73635e1297a721e167cf59eb9bbc06a219ea7ccd
gauharb <gauhara.baimagambet@gmail.com>
2026-09-13 15:19:32 +0500

    Add fields

--- src/main/java/travel/TravelPackage.java
+++ src/main/java/travel/TravelPackage.java
@@ -1,4 +1,11 @@
 package travel;
 
 public class TravelPackage {
+
+    private String destination;
+    private String hotel;
+    private String flight;
+    private String transport;
+    private String meals;
+    private String activities;
 }
