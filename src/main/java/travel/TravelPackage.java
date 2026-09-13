8272199623b2605206486d098866b522baf86e14
gauharb <gauhara.baimagambet@gmail.com>
2026-09-13 15:29:59 +0500

    Add setters & toString().finish

--- src/main/java/travel/TravelPackage.java
+++ src/main/java/travel/TravelPackage.java
@@ -8,4 +8,39 @@ public class TravelPackage {
     private String transport;
     private String meals;
     private String activities;
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public void setFlight(String flight) {
+        this.flight = flight;
+    }
+
+    public void setTransport(String transport) {
+        this.transport = transport;
+    }
+
+    public void setMeals(String meals) {
+        this.meals = meals;
+    }
+
+    public void setActivities(String activities) {
+        this.activities = activities;
+    }
+
+    @Override
+    public String toString() {
+        return "Travel Package\n" +
+                "Destination: " + destination + "\n" +
+                "Hotel: " + hotel + "\n" +
+                "Flight: " + flight + "\n" +
+                "Transport: " + transport + "\n" +
+                "Meals: " + meals + "\n" +
+                "Activities: " + activities;
+    }
 }
