dfa90f1c1d56979a428818c4353ae12475554be2
gauharb <gauhara.baimagambet@gmail.com>
2026-09-13 16:40:48 +0500

    Add getters cause needed for defaults/validation logic

--- src/main/java/travel/TravelPackage.java
+++ src/main/java/travel/TravelPackage.java
@@ -13,24 +13,48 @@ public class TravelPackage {
         this.destination = destination;
     }
 
+    public String getDestination() {
+        return destination;
+    }
+
     public void setHotel(String hotel) {
         this.hotel = hotel;
+    }
+
+    public String getHotel() {
+        return hotel;
     }
 
     public void setFlight(String flight) {
         this.flight = flight;
     }
 
+    public String getFlight() {
+        return flight;
+    }
+
     public void setTransport(String transport) {
         this.transport = transport;
+    }
+
+    public String getTransport() {
+        return transport;
     }
 
     public void setMeals(String meals) {
         this.meals = meals;
     }
 
+    public String getMeals() {
+        return meals;
+    }
+
     public void setActivities(String activities) {
         this.activities = activities;
+    }
+
+    public String getActivities() {
+        return activities;
     }
 
     @Override
